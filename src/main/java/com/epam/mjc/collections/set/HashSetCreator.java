5e7eb08920935bf4b12219b9e701b3f0b2bce7be
Max <maxageev5@gmail.com>
2022-11-28 11:15:15 +0300

    1

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,18 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+         HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int x : sourceList) {
+            if (x % 2 != 0) {
+                hashSet.add(x);
+                hashSet.add(x * 2);
+            }
+            while (x % 2 == 0) {
+                hashSet.add(x);
+                hashSet.add(x /= 2);
+            }
+        }
+        return hashSet;
     }
 }
